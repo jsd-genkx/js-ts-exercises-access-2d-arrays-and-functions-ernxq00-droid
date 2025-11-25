ac3d79c57fd64fbd637a0328c8284ccfd2d918cc
Chanansiri Kumyon <ernxq00@gmail.com>
2025-11-25 09:08:50 +0700

    hw:fn

--- game.ts
+++ game.ts
@@ -9,17 +9,19 @@ const moves = [];
 // Start at A (0,0) -> [row][col]
 let row = 0;
 let col = 0; 
-moves.push(board[row][col]); // A
-
+moves.push(board[row][col]); // A //r0 c0 [0][0]
 // Move Right to B
 col++;
 moves.push(board[row][col]); // B
-
 // Move Right to C
-
+col++;
+moves.push(board[row][col]);
 // Move Down to F
-
+row++;
+moves.push(board[row][col]);
 // Move left to E
+col--;
+moves.push(board[row][col]);
 
 console.log("Path:", moves.join(" → "));
 // Output: Path: A → B → C → F → E
